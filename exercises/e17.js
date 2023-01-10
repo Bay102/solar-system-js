f0943b08c0acb827ca734feed474ac71cef62420
Zak Bay <zacharybay@live.com>
2023-01-09 19:03:17 -0800

    e17 was not correct, now it is

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,28 +1,26 @@
-// const callBack = (person) => person.age;
+const people = [
+  // { name: "jon", age: 1},
+  // { name: "peter", age: 2 },
+  // { name: "andrey", age: 3 },
+  // { name: "rachel", age: 4 },
+];
 
-// const people = [
-//   { name: "jon", age: 29 },
-//   { name: "peter", age: 30 },
-//   { name: "andrey", age: 22 },
-//   { name: "rachel", age: 25 },
-// ];
-
-export function minBy(array, cb) {  
-  for (const element of array) {
-    let personAge = array[0]; 
-    if (cb(element) < personAge.age) {
-      return element
+export function minBy(array, cb) {
+  let minAge = array[0]; 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].age < cb(minAge)) {
+       minAge = array[i]; 
+      }
     }
-  }
+    return minAge
 }
-  
-
 
 export function maxBy(array, cb) {
-  for (const element of array) {
-    let personAge = array[0]; 
-    if (cb(element) > personAge.age) {
-      return element
+  let maxAge = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].age > cb(maxAge)) {
+      maxAge = array[i];
     }
   }
+  return maxAge
 }
